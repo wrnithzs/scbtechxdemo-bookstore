78647cd91cf199aebac2247fdb85997cf63c7ad1
worranittha <wrnith_zs@hotmail.com>
2021-11-07 02:48:37 +0700

    initial spring boot app

---
+++ src/main/java/com/example/scbtechxdemo/ScbtechxdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scbtechxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbtechxdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScbtechxdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scbtechxdemo/ScbtechxdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scbtechxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbtechxdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
