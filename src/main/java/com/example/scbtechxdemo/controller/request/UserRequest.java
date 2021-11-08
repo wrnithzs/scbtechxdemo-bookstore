84a7c6663d88cd4fac5363c5ed8d618f0745a4ca
worranittha <wrnith_zs@hotmail.com>
2021-11-08 20:29:31 +0700

    add user api

--- src/main/java/com/example/scbtechxdemo/controller/request/UserRequest.java
+++ src/main/java/com/example/scbtechxdemo/controller/request/UserRequest.java
@@ -1,5 +1,10 @@
 package com.example.scbtechxdemo.controller.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +13,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class UserRequest {
-
+	@NotBlank
+	@Size(min = 1, max = 100)
 	private String username;
+	@NotBlank
+	@Length(min = 6, message = "The field must be at least {min} characters")
 	private String password;
 	private String name;
 	private String surname;
